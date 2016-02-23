a8e6a8d3c38a5fb1c277324c8881248547201ecf
mulfordjc <mulfordjc@appstate.edu>
2016-02-23 16:59:18 -0500

    Made first step to using synonym sets

--- sklearn_naive_bayes/functions.py
+++ sklearn_naive_bayes/functions.py
@@ -1,2 +1,9 @@
+from nltk.corpus import wordnet as wn
+
 def get_synonym_sets(word):
-    pass
+    synonym_set = []
+    synonyms = wn.synsets(word)
+    for syn in synonyms:
+        for lemma in syn.lemmas():
+                synonym_set.append(lemma.name())
+    return synonym_set
