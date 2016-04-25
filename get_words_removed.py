46ee4d8df0855f06a493bea98541ec2e084885d6
mulfordjc <mulfordjc@appstate.edu>
2016-04-24 21:43:28 -0400

    Cleaned up main. Sorted word_list by threshold, changed threshold to be max instead of min

--- get_words_removed.py
+++ get_words_removed.py
@@ -13,9 +13,6 @@ from math import log
 from sklearn_naive_bayes import make_2d_array, post_filter_functions
 
 
-MAX = 500
-
-
 def make_threshold(counts, message_length):
     """
     Takes the counts dict and creates the threshold of removal for each word
@@ -31,7 +28,7 @@ def make_threshold(counts, message_length):
             all_messages[1] += count[key][1]
 
         #For each category, compute the needed values and determine if it fits the threshold
-        lowest_threshold = MAX
+        best_threshold = 0
         for category in count.keys():
             cat_arr = count[category]
             occurr_in_cat = cat_arr[1] + 1
@@ -40,11 +37,11 @@ def make_threshold(counts, message_length):
             messages_out_cat = (all_messages[1] - cat_arr[1]) + (all_messages[0] - cat_arr[0])
 
             curr_threshold = abs(log( (occurr_in_cat/messages_in_cat) / (occurr_out_cat/messages_out_cat)))
-            if curr_threshold < lowest_threshold:
-                lowest_threshold = curr_threshold
+            if curr_threshold > best_threshold:
+                best_threshold = curr_threshold
 
 
-        counts_threshold.append(lowest_threshold)
+        counts_threshold.append(best_threshold)
 
     return counts_threshold
 
@@ -53,6 +50,8 @@ def make_word_array(index_dict, count_thresholds):
     """
     Takes the index dictionary {word: index} and creates an array
     where arr[index] -> (word, threshold)
+
+    Sorts by threshold
     """
     word_array = ['']*len(index_dict)
 
@@ -60,22 +59,27 @@ def make_word_array(index_dict, count_thresholds):
         index = index_dict[word]
         word_array[index] = (word, count_thresholds[index])
 
-    return word_array
- 
+    return sorted(word_array, key=lambda x: x[1])
 
-def main(data_files, threshold_start, threshold_end, threshold_increment, output_file):
-    # Get counts, index_dict
+
+def get_counts_and_index_dict(data_files):
+    """
+    Creates the count and index_dict structures for the datafiles
+    """
     unique_words, messages, categories = make_2d_array.make_messages(data_files)
     index_dict = make_2d_array.make_index_dict(unique_words, None)
     arrays = make_2d_array.make_2d_arrays(index_dict, messages, None)
-
     counts, message_length = post_filter_functions.make_counts(arrays, categories)
-    # Make threshold for each count
+
+    return counts, index_dict, message_length
+    
+
+def main(data_files, threshold_start, threshold_end, threshold_increment, output_file):
+    counts, index_dict, message_length = get_counts_and_index_dict(data_files)
     count_thresholds = make_threshold(counts, message_length)
-    # Turn counts to array (for immediate lookup)
     word_array = make_word_array(index_dict, count_thresholds)
+
     print(word_array)
-    # Go through each threshold and print to file when that word will be removed
 
 
 if __name__ == "__main__":
